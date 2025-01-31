4352f9feef8c3d3a74bbd188b7f5aad583a397fc
Henry <sebasmora.0322.smm@gmail.com>
2025-01-31 10:10:50 -0600

    summit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 20, 2025 12:00:00").getTime();
+var countDownDate = new Date("Feb 20, 2025 12:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
